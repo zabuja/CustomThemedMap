50c4000cc11684687994fe565754b4906fca5241
zabuja <atanu.tmsl@gmail.com>
2021-09-05 15:20:30 +0530

    structure made, marker manipulation left

---
+++ main.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 23.567619641501377, lng: 87.29866731812979},
+        zoom: 14,
+        mapId: '1cf394bcd4e863dc'
+      });
+}
